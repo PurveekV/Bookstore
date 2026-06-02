2bc9e624db03e0ea96fb08a4b2e07bf74abb34bd
PurveekV <purveekvipul77@gmail.com>
2026-06-02 16:50:36 +0100

    New feature updates for 2 database tables

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,43 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const db = require('./db');
 
 const app = express();
+app.use('/images', express.static(path.join(__dirname, 'Database/images')));
 app.use(cors());
 app.use(express.json());
 
 /* GET ALL BOOKS */
 app.get('/books', async (req, res) => {
-    db.query('SELECT * FROM books', (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: 'Database query failed' });
-        }
-        res.json(results);
+    db.query(
+        `SELECT 
+            b.title,
+            b.price,
+            b.category,
+            b.image,
+            a.name AS author_name
+         FROM books b
+         JOIN authors a
+            ON b.author_id = a.author_id`,
+        (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database query failed' });
+            }
+            const books = results.map(book => ({
+                ...book,
+                image: `http://localhost:3000/images/${book.image}`
+            }));
+
+            res.json(books);
+        });
     });
-});
 
 /* SEARCH BOOKS */
 app.get('/books/search/:keyword', (req, res) => {
     const keyword = `%${req.params.keyword}%`;
 
-    db.query('SELECT * FROM books WHERE title LIKE ? OR author LIKE ?', [keyword, keyword], (err, results) => {
+    db.query('SELECT * FROM books WHERE title LIKE ? OR authors LIKE ?', [keyword, keyword], (err, results) => {
         if (err) {
             return res.status(500).json({ error: 'Database query failed' });
         }
@@ -32,13 +49,13 @@ app.get('/books/search/:keyword', (req, res) => {
 app.post('/books', (req, res) => {
     const {
         title,
-        author,
+        author_name,
         price,
         category,
         image
     } = req.body;
 
-    db.query('INSERT INTO books (title, author, price, category, image) VALUES (?, ?, ?, ?, ?)', [title, author, price, category, image], (err, results) => {
+    db.query('INSERT INTO books (title, author_id, price, category, image) VALUES (?, ?, ?, ?, ?)', [title, author_name, price, category, image], (err, results) => {
         if (err) {
             return res.status(500).json({ error: 'Database query failed' });
         }
