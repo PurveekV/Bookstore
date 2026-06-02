5a02943dc5163b54d74ad98dc3064bfd93531fc6
PurveekV <purveekvipul77@gmail.com>
2026-06-02 14:34:50 +0100

    Updated db.js with login data from .env

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345',
-    database: 'bookstore'
-});
+const config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME
+};
+const connection = mysql.createConnection(config);
 
 connection.connect((err) => {
     if (err) {
